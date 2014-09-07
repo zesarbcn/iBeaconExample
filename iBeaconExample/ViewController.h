f731039fb20fe704e6dc7bf6022b8113e4aa478d
Cesar Perez <zesarbcn@gmail.com>
2014-09-07 13:44:51 +0200

    Initial Commit

---
+++ iBeaconExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iBeaconExample
+//
+//  Created by Cesar Perez Laguna on 07/09/14.
+//  Copyright (c) 2014 Cesar Perez Laguna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iBeaconExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iBeaconExample
+//
+//  Created by Cesar Perez Laguna on 07/09/14.
+//  Copyright (c) 2014 Cesar Perez Laguna. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
